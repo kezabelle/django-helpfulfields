0ffbd2c76327a0d9b7738da9305e33c28006d6d2
Keryn Knight <keryn@kerynknight.com>
2013-02-03 17:59:02 +0000

    titles combo: a title and a menu title field. TODO: labels & help

--- helpfulfields/models.py
+++ helpfulfields/models.py
@@ -7,3 +7,19 @@ class ChangeTracking(models.Model):
 
     class Meta:
         abstract = True
+
+
+
+class Titles(models.Model):
+    title = models.CharField(max_length=255, verbose_name=titles_title_label)
+    menu_title = models.CharField(max_length=255, blank=True,
+        verbose_name=titles_menu_label, help_text=titles_menu_help)
+
+    def get_menu_title(self):
+        if self.menu_title:
+            return self.menu_title
+        return self.title
+
+    class Meta:
+        abstract = True
+
