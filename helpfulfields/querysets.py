654eb7388cb0a560e740a2c6672b5af1c9aefcd1
Keryn Knight <keryn@kerynknight.com>
2013-02-18 22:08:09 +0000

    new SoftDeleteQuerySet. In theory.

--- helpfulfields/querysets.py
+++ helpfulfields/querysets.py
@@ -98,3 +98,40 @@ class DatePublishingQuerySet(QuerySet):
         """
         now = datetime.now()
         return self.filter(Q(unpublish_on__lte=now) | Q(publish_on__gte=now))
+
+
+class SoftDeleteQuerySet(QuerySet):
+    """
+    A custom queryset which goes hand in hand with the `SoftDelete` model to
+    provide a way to filter by the additional field that creates.
+    """
+
+    def all(self):
+        """ Finds all objects which haven't been marked as deleted. This
+        includes those which have been restored previously.
+        :return: objects which haven't been marked as deleted
+        :rtype: :class:QuerySet
+        """
+        restored_val = self.model.DELETED_CHOICES[1][0]
+        return super(SoftDeleteQuerySet, self).all().filter(
+            Q(deleted__isnull=True) | Q(deleted=restored_val)
+        )
+
+    def deleted(self):
+        """ filters the queryset looking for deleted items only.
+        :return: objects which are currently deleted
+        :rtype: :class:QuerySet
+        """
+        deleted_val = self.model.DELETED_CHOICES[2][0]
+        return self.filter(deleted=deleted_val)
+
+    def restored(self):
+        """ filters the queryset looking for objects which have been previously
+        deleted, and since restored.
+        :return: objects which were once deleted.
+        :rtype: :class:QuerySet
+        """
+        restored_val = self.model.DELETED_CHOICES[1][0]
+        return self.filter(deleted=restored_val)
+
+
