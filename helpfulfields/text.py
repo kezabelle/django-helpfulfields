120ef406a7fd6b180aa3d96023cd6729fd2d20a7
Keryn Knight <keryn@kerynknight.com>
2013-02-18 22:07:40 +0000

    additional code missing from previous commit :\

--- helpfulfields/text.py
+++ helpfulfields/text.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # All the text strings we need to mark for translation go in here.
-# By putting them here, we can keep everything nice and tidy, and, bonus, we don't
-# have to jump through long-winded hoops to get the verbose_name from a Model
-# to be the label for an overridden Form field.
+# By putting them here, we can keep everything nice and tidy, and, bonus, we
+# don't have to jump through long-winded hoops to get the verbose_name from a
+# Model to be the label for an overridden Form field.
 from django.utils.translation import ugettext_lazy as _
 
 titles_title_label = _(u'title')
@@ -26,6 +26,10 @@ soft_delete_false = _(u'restored')
 soft_delete_true = _(u'deleted')
 
 changetracking_fieldset_label = _(u'changes')
+created_label = _(u'created')
+created_help = _(u'the date and time on which this object was first added')
+modified_label = _(u'last modified')
+modified_help = _(u'the date and time on which this object was last changed')
 
 dates_fieldset_label = _(u'publishing info')
 
@@ -34,9 +38,14 @@ quick_publish_help = _(u'should this object be visible on the website')
 
 publish_label = _(u'publishing date')
 publish_help = _(u'the date and time on which this object should be visible on '
-    u'the website.')
+                 u'the website.')
 unpublish_label = _(u'publishing end date')
-unpublish_help = _(u'if filled in, this date and time are when this object will '
-    u'cease being available.')
+unpublish_help = _(u'if filled in, this date and time are when this object '
+                   u'will cease being available.')
+
+object_lacks_pk = _(u"%(model)s object can't be deleted because its %(pk)s "
+                    u"attribute is set to None.")
+object_not_deleted = _(u"%(model)s object can't be restored because it has not "
+                       u"been deleted.")
 
 view_on_site_label = _(u'View on site')
--- helpfulfields/utils.py
+++ helpfulfields/utils.py
@@ -3,6 +3,14 @@ from datetime import datetime
 
 
 def datediff(original, minutes=30):
+    """
+    Compares a given datetime to now to find out if it was recent.
+
+    :param original: the original date
+    :param minutes: how long ago is considered recent
+    :return: whether or not it's recent
+    :rtype: boolean
+    """
     if not original:
         return False
     timediff = datetime.now() - original
